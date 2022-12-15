f961ac3822b5c732b6ebb8096683b5040a9a23d9
Shazammon <ammonorgill@gmail.com>
2022-12-14 18:43:46 -0700

    documented pseudo code for function

--- soluion.js
+++ soluion.js
@@ -7,3 +7,8 @@ let data = fs.readFileSync('data.txt', 'utf-8')
 const stringData = data.split(/\r?\n/)
 
 console.log(stringData)
+
+// for each array item:
+// compare first number to second number
+// whichever number is smaller, assign it "lowest" value. If values are equal, assign
+// compare the upper value of elf with lowest value to the upper value
